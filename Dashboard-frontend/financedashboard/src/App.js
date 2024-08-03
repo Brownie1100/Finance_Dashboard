37bbc2e483617bd7464f916928a9e0bf090dda1b
Brownie1100 <90987118+Brownie1100@users.noreply.github.com>
2024-08-04 01:16:40 +0530

    34
    wrra

--- Dashboard-frontend/financedashboard/src/App.js
+++ Dashboard-frontend/financedashboard/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import getHelloMessage from './api.js';
+import './App.css';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -19,9 +20,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <div className="App-header">
+        {/* <h1>FINANCE DASHBOARD</h1> */}
+        <h1>DASHBOARD</h1>
+      </div>
+
+      <div className="sheets">
+        <div className="Dashboard">
+          <h1>{message}</h1>
+        </div>
+
+        <div className="Income">
+          <h1>{message}</h1>
+        </div>
+
+        <div className="Expense">
+          <h1>{message}</h1>
+        </div>
+      </div> 
     </div>
   );
-}
+} 
 
 export default App;
